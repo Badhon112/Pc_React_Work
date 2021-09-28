1951c2ea88a329b0255aa8f3b6b731191ed14645
Badhon Biswas <biswasbadhon896@gmail.com>
2021-09-27 22:16:03 -0800

    Very Imp Tropic Map Route

--- secound-app/src/Class/App.js
+++ secound-app/src/Class/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from "react";
 import Navbar from "./navbar";
 import News from "./News";
-
+import Map from "./Map";
 export default class App extends Component {
   render() {
     return (
       <div className="container">
         <Navbar />
-        <News />
+        {/* <News /> */}
+        <Map />
       </div>
     );
   }
---
+++ secound-app/src/Class/Map.js
@@ -0,0 +1,46 @@
+import React, { Component } from "react";
+import img1 from "./Image/1.jpg";
+import img2 from "./Image/2.jpg";
+import img3 from "./Image/3.jpg";
+import img4 from "./Image/4.jpg";
+export default class map extends Component {
+  render() {
+    const List = [
+      {
+        img: img1,
+        title: "Friend",
+        Decripe: "Something",
+      },
+      {
+        img: img2,
+        title: "Not Friend",
+        Decripe: "Something",
+      },
+      {
+        img: img3,
+        title: "Best Friend Friend",
+        Decripe: "Something",
+      },
+      {
+        img: img4,
+        title: "No Friend",
+        Decripe: "Something",
+      },
+    ];
+    return (
+      <div className="container">
+        {List.map((Key) => {
+          const { img, title, Decripe } = Key;
+          return (
+            <div>
+              {/* <img src={img} alt="There is A Image" sizes="20px" srcset="" /> */}
+              <img src={img} alt="There is A image" width="100px" />
+              <h3>{title}</h3>
+              <h3>{Decripe}</h3>
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
+}
